e6a46231c0ad2c80ebc919a60c1a1707c6a5d3f6
user <enyukpavlo@gmail.com>
2018-05-08 09:05:50 -0400

    word list moved to sepperate file

--- bot.py
+++ bot.py
@@ -1,41 +1,12 @@
 import tweepy
-from credentials import *
+from credentials import *	#your twitter auth keys and tokens
+from dict_terms import *	#word filer dict
 from time import sleep
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
-
-#dicionary of terms to be subsituted
-dic = {'Senator': 'Elf-Lord',
-		'shirt': 'robe',
-		'Shirt': 'Robe',
-		'Attorney': 'Paladin',
-		'attorney': 'paladin',
-		'officer': 'knight-errant',
-		'Officer': 'Knight-errant',
-		'Electriciry': 'Magic',
-		'electricity': 'magic',
-		'Technology': 'Mage-craft',
-		'technology': 'mage-craft',
-		'Mayor': 'Minor noble',
-		'mayor': 'minor noble',
-		'Gun': 'Boomstick',
-		'gun': 'boomstick',
-		'Knife': 'Stabby thing',
-		'knife': 'stabby thing',
-		'judge': 'big wig man',
-		'investigation': 'whodunnit',
-		'witnesses': 'these dudes I know',
-		'Russia': 'Land of Ice and Snow',
-		'Car': 'Chariot',
-		'Dog': 'Doggo',
-		'dog': 'doggo',
-		'Person': 'Human',
-		'person': 'human',
-		'People': 'Humans',
-		'people': 'humans',}
 
 #begin boot loop, opens and writes to the txt file that will hold the last tweet of targeted user
 #encodes xyz-tweets.txt in UTF8 format since ASCI can't deal with emojis
